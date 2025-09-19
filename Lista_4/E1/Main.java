768ef787dde4bb095874e482ce66662be3afc83f
Wesley SantAnna <whbsantanna@gmail.com>
2025-09-18 22:53:43 -0300

    Adiciona arquivos da Lista_4

---
+++ Lista_4/E1/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Shape> shapes = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < 12; i++) {
+            if (random.nextBoolean()) {
+                shapes.add(new Circle(1 + random.nextInt(10)));
+            } else {
+                shapes.add(new Square(1 + random.nextInt(10)));
+            }
+        }
+
+        int index = 1;
+        for (Shape s : shapes) {
+            System.out.println("Forma " + index + " área = " + s.getArea());
+            index++;
+        }
+    }
+}
